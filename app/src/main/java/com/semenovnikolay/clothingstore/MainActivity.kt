8beeda34bd7b0d0069a288400f2553146c47e82a
Nikolui333 <semenovnikolu@yandex.ru>
2022-06-06 17:59:23 +0300

    bottom_menu

--- app/src/main/java/com/semenovnikolay/clothingstore/MainActivity.kt
+++ app/src/main/java/com/semenovnikolay/clothingstore/MainActivity.kt
@@ -2,10 +2,36 @@ package com.semenovnikolay.clothingstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.semenovnikolay.clothingstore.databinding.ActivityMainBinding
+import com.semenovnikolay.clothingstore.presentation.Tabs.AddClothes
+import com.semenovnikolay.clothingstore.presentation.Tabs.Favorite
+import com.semenovnikolay.clothingstore.presentation.Tabs.ShoppingCart
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // запуск MaterialToolbar в activity_main.xml
+        setSupportActionBar(binding?.topMainMenu)
+
+        supportFragmentManager.beginTransaction().replace(R.id.mainContent, AddClothes()).commit()
+
+        binding?.bottomMainMenu?.setOnItemSelectedListener { item ->
+
+            when(item.itemId) {
+                R.id.addClothesBottomMainMenu -> supportFragmentManager.beginTransaction().replace(R.id.mainContent, AddClothes()).commit()
+                R.id.favoritesBottomMainMenu -> supportFragmentManager.beginTransaction().replace(R.id.mainContent, Favorite()).commit()
+                R.id.shoppingCardBottomMainMenu -> supportFragmentManager.beginTransaction().replace(R.id.mainContent, ShoppingCart()).commit()
+            }
+
+            return@setOnItemSelectedListener true
+        }
+        binding?.bottomMainMenu?.selectedItemId = R.id.addClothesBottomMainMenu
     }
 }
---
+++ app/src/main/java/com/semenovnikolay/clothingstore/presentation/App.kt
@@ -0,0 +1,26 @@
+package com.semenovnikolay.clothingstore.presentation
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            // Koin Android logger
+            // в androidLogger задаётся то, насколько подробно нужно выводить оишбки
+            androidLogger(Level.ERROR)
+            //inject Android context
+            androidContext(this@App)
+
+            modules()
+
+        }
+
+    }
+}
